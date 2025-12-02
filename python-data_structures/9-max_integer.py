44d06711130354292ac748da1798946a99ca6af4
hexatronn <hikmtmmmdov26@gmail.com>
2025-12-02 12:20:05 +0000

    Find the max

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#1/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None;
+    
+    max_val = my_list[0]
+    for num in range:
+        if num > max_val:
+            max_val = num
+    return max_val
