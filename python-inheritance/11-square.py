a1fd3f8b9e98e8bb1672dd131e8d139295cb5fbc
hexatronn <hikmtmmmdov26@gmail.com>
2025-12-05 12:27:05 +0000

    Square #2

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Square class that inherits from Rectangle."""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class."""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Return area of the square."""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return string representation of the square."""
+        return f"[Square] {self.__size}/{self.__size}"
