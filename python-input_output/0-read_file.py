e513c9d0f6acb8b47791f8841b0fb61341d816ae
hexatronn <hikmtmmmdov26@gmail.com>
2025-12-06 10:53:27 +0000

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Module that reads and prints a text file.""" 
-def read_file(filename=""):
-        """Reads a UTF-8 text file and prints it to stdout."""
-	with open('my_file_0.txt', 'r', encoding="utf-8") as f:
-	print(f.read())
+"""Module that reads and prints a text file."""
 
+
+def read_file(filename=""):
+    """Reads a UTF-8 text file and prints it to stdout."""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
