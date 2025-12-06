e6e322c0239b2bd3b4f3df2f4eba48686c097b1e
hexatronn <hikmtmmmdov26@gmail.com>
2025-12-06 11:00:00 +0000

    Write to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """Writes a UTF-8 text file and prints it to stdout."""
     with open(filename, "w", encoding="utf-8") as f:
-        print(f.write(), end="")
+        return f.write(text)
