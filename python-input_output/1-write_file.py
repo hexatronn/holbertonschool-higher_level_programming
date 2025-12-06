4898ed2d7709b4fbcc98720a3b2d21983f7f9cc2
hexatronn <hikmtmmmdov26@gmail.com>
2025-12-06 10:55:42 +0000

    Write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that writes and prints a text file."""
+
+
+def read_file(filename=""):
+    """Writes a UTF-8 text file and prints it to stdout."""
+    with open(filename, "w", encoding="utf-8") as f:
+        print(f.write(), end="")
