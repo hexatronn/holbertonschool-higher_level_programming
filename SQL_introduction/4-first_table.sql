006256dc02f30d8cdb2da197a6ecda83d2aeb129
hexatronn <hikmtmmmdov26@gmail.com>
2025-12-09 11:50:30 +0000

    First Table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Creates the table first_table in the current database
+-- Does not fail if the table already exists
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
