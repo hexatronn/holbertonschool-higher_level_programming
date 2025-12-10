3914c512888d5209fad3e3e1eaf1952b9331342b
hexatronn <hikmtmmmdov26@gmail.com>
2025-12-10 10:09:07 +0000

    Privileges

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Lists all privileges of MySQL users user_0d_1 and user_0d_2
+-- Using SHOW GRANTS to display privileges
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+Hi! It's a README file of SQL - More queries !
