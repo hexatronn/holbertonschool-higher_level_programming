44cfb47bd833ae99ba016f9bfea67a6890174915
hexatronn <hikmtmmmdov26@gmail.com>
2025-12-11 13:59:17 +0000

    States table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+-- Create DB
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use DB
+USE hbtn_0d_usa;
+
+-- Create table
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
