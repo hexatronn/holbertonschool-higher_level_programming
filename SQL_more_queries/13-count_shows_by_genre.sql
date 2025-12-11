8ab474571b4bafc478864259a841f47cbfda95ad
hexatronn <hikmtmmmdov26@gmail.com>
2025-12-11 14:18:22 +0000

    Number of shows by genre

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Count shows per genre
+
+-- Select genre name and count
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.id
+ORDER BY number_of_shows DESC;
