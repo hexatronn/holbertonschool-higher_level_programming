9337cbb14341ce23b74fa40ce4466cf100dbb232
hexatronn <hikmtmmmdov26@gmail.com>
2025-12-16 10:56:42 +0000

    Search API

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Sends a POST request with a letter and processes a JSON response."""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+
+    response = requests.post(url, data={"q": q})
+
+    try:
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data.get("id"), data.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
